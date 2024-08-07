82ab6dcece732ad25e12d65eb80d2ceb1cd82bc3
Alexandre D'Ambrisi <alexandre.dambrisi@gmail.com>
2024-08-07 13:36:49 +0200

    Create network manager and call to get repository infos

---
+++ RepoWatcher/RepoWatcher/NetworkManager.swift
@@ -0,0 +1,47 @@
+//
+//  NetworkManager.swift
+//  RepoWatcher
+//
+//  Created by Alexandre D'AMBRISI on 07/08/2024.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private let decoder = JSONDecoder()
+    
+    private init() {
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    
+    func getRepo(atUrl urlString: String) async throws -> Repository {
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidRepoUrl
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let safeResponse = response as? HTTPURLResponse, safeResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        do {
+            return try decoder.decode(Repository.self, from: data)
+        } catch {
+            throw NetworkError.invalidRepoData
+        }
+    }
+}
+
+enum NetworkError: Error {
+    case invalidRepoUrl
+    case invalidResponse
+    case invalidRepoData
+}
+
+enum RepoURL {
+    static let widgetKit = "https://api.github.com/repos/dambri/ios-widgetkit"
+    static let discordBot = "https://api.github.com/repos/dambri/sagira-discord-bot"
+}
