a4e6f312175366121985b2426d55086853360057
Alexandre D'Ambrisi <alexandre.dambrisi@gmail.com>
2024-08-12 11:00:59 +0200

    Create calendar UI

--- SwiftCal/SwiftCal/ContentView.swift
+++ SwiftCal/SwiftCal/ContentView.swift
@@ -15,14 +15,37 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Day.date, ascending: true)],
         animation: .default)
     private var days: FetchedResults<Day>
+    private let daysOfWeek = Calendar.current.veryShortWeekdaySymbols
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(days) { day in
-                    Text(day.date!.formatted())
+            VStack {
+                HStack {
+                    ForEach(daysOfWeek, id: \.self) { dayOfWeek in
+                        Text(dayOfWeek)
+                            .fontWeight(.black)
+                            .foregroundStyle(.orange)
+                            .frame(maxWidth: .infinity)
+                    }
                 }
+                
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7), content: {
+                    ForEach(days) { day in
+                        Text(day.date!.formatted(.dateTime.day()))
+                            .fontWeight(.bold)
+                            .foregroundStyle(day.didStudy ? .orange : .secondary)
+                            .frame(maxWidth: .infinity, minHeight: 40)
+                            .background(
+                                Circle()
+                                    .foregroundStyle(.orange.opacity(day.didStudy ? 0.3 : 0.0))
+                            )
+                    }
+                })
+                
+                Spacer()
             }
+            .navigationTitle(Date().formatted(.dateTime.month(.wide)))
+            .padding()
         }
     }
 }
